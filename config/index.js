19ddd15e7b9c5ce49477baef2ada9d8f425e8637
Hitoki <melendrezomar2@gmail.com>
2021-07-28 01:30:42 -0300

    fix db connection

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports ={
     port: 3001,
     omdbApiUrl: "http://www.omdbapi.com/?apikey=7bbb11a0",
     DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_HOST,
-    DB_USER: process.env.DB_HOST,
-    DB_PASSWORD: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
 }
